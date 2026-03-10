6f5698f17e166a9682ceca55f9ca99573878f92a
ITMustang <ITmustang@users.noreply.github.com>
2026-03-10 11:36:43 -0400

    Update hydrate-2025.sh

--- hydrate-2025.sh
+++ hydrate-2025.sh
@@ -425,7 +425,7 @@ case $choice in
 
         blue_echo "Configuring and Starting TailScale..."
         sudo systemctl enable --now tailscaled
-        sudo tailscale up --advertise-tags=tag:ssh
+        sudo tailscale up --ssh --advertise-tags=tag:ssh
   
         # ----- install nessus ------
         blue_echo "Installing Tenable Nessus..."
